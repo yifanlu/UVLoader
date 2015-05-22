99d6585da5f4d9cf3137b31abfbb67f236a6682c
Yifan Lu <me@yifanlu.com>
2015-05-21 19:58:21 -0700

    Added signed types and fixed u64_t type to be correct size

--- types.h
+++ types.h
@@ -11,7 +11,11 @@
 typedef unsigned char u8_t;             ///< Unsigned 8-bit type
 typedef unsigned short int u16_t;       ///< Unsigned 16-bit type
 typedef unsigned int u32_t;             ///< Unsigned 32-bit type
-typedef unsigned long int u64_t;        ///< Unsigned 64-bit type
+typedef unsigned long long int u64_t;        ///< Unsigned 64-bit type
+typedef char s8_t;             ///< Signed 8-bit type
+typedef short int s16_t;       ///< Signed 16-bit type
+typedef int s32_t;             ///< Signed 32-bit type
+typedef long long int s64_t;        ///< Signed 64-bit type
 /** @}*/
 
 /** \name SCE standard types
